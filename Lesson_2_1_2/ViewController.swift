13b41f234921e464a48a6b76e4f99bd408bc52c3
Apl064 <apl_064@me.com>
2022-03-01 22:42:56 +0300

    Initial Commit

---
+++ Lesson_2_1_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2_1_2
+//
+//  Created by Pavel Metelin on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
