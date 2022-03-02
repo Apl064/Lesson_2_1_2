111231f79e209802af1f4883bf66c9540f88da24
Apl064 <apl_064@me.com>
2022-03-02 03:35:36 +0300

    Indicator Logic

--- Lesson_2_1_2/ViewController.swift
+++ Lesson_2_1_2/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var indicatorViews: [UIView]!
+    @IBOutlet var switchButton: UIButton!
+    
+    var clickCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for indicatorView in indicatorViews {
+            indicatorView.layer.cornerRadius = indicatorView.frame.height / 2
+            indicatorView.alpha = 0.3
+        }
+        
+        switchButton.layer.cornerRadius = 10
     }
+    
+    @IBAction func PressSwitchButton() {
+        switchButton.setTitle("NEXT", for: .normal)
 
-
+        switch clickCount {
+        case 0:
+            indicatorViews[clickCount].alpha = 1
+            indicatorViews[clickCount+2].alpha = 0.3
+            clickCount +=  1
+        case 1:
+            indicatorViews[clickCount].alpha = 1
+            indicatorViews[clickCount-1].alpha = 0.3
+            clickCount +=  1
+        case 2:
+            indicatorViews[clickCount].alpha = 1
+            indicatorViews[clickCount-1].alpha = 0.3
+            clickCount = 0
+        default:
+            for indicatorView in indicatorViews {
+                indicatorView.alpha = 0.3
+            }
+        }
+    }
+    
 }
 
